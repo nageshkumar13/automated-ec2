8390db1845a07118668a3eb37c2b9a7b0761abaf
nageshkumar13 <nageshkumar1925@gmail.com>
2024-06-04 13:32:05 +0530

    change compute.tf file

--- ec2-automation/compute.tf
+++ ec2-automation/compute.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "ec2_9_5" {
 }
 
 resource "aws_instance" "ec2_24_7" {
-  ami = "ami-00fa32593b478ad6e" # Amazon Linux 2023 AMI, eu-west-3
+  ami = "ami-00fa32593b478ad6e" # Amazon Linux 2023 AMI, ap-south-1
   instance_type = "t2.micro"
   tags = {
     Name = "EC2 24h"
